3ac2ffed12686c463baf744028096c73f0bc5a71
TradersDen Developer <developer@tradersden.site>
2026-02-26 03:22:18 +0300

    Initial commit: Estate Traders platform with special demo account VRTC7528369

---
+++ packages/bot-skeleton/src/services/api/appIdResolver.js
@@ -0,0 +1,9 @@
+const AppIds = Object.freeze({
+    'deriv.app': '128700', // TODO: [app-link-refactor] - Remove backwards compatibility for `deriv.app`
+    'staging.deriv.app': '128700',
+    'app.deriv.com': '128700',
+    'staging-app.deriv.com': '128700',
+    'goon-puce.vercel.app': '128700',
+});
+
+export default AppIds;
